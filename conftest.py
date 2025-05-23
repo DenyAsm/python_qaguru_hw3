fc5219ae26427982a7c859969d2b3ba5fa90251d
Denis Asmankin <denis.asmankin@gmail.com>
2025-05-23 20:09:02 +0500

    stop -> go friday

--- conftest.py
+++ conftest.py
@@ -1,18 +1,19 @@
+
 import pytest
 from selene import browser, have
 
-@pytest.fixture
+@pytest.fixture # по дефолту scope = "function"
 def log_test():
     print("\nTEST_start")
     yield
     print("TEST_end")
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function") # опционально, по дефолту in
 def window_browser_mode():
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    print("OPEN - Window size mode")
+    browser.config.window_width = 2560
+    browser.config.window_height = 1440
+    print("OPEN - size mode")
     yield
     browser.quit()
     print("\nCLOSE")
@@ -21,12 +22,14 @@ def window_browser_mode():
 def open_google(window_browser_mode):
     browser.open('https://google.com')
     browser.should(have.script_returned(True, 'return document.readyState === "complete"'))
-    print("Open main Google")
+
+    print("Open main - Google")
     yield
 
 @pytest.fixture
-def open_other_browser(window_browser_mode):
+def open_duck(window_browser_mode):
     browser.open('https://duckduckgo.com/')
     browser.should(have.script_returned(True, 'return document.readyState === "complete"'))
+
     print('Open other site - Duck')
     yield
--- test_search_google.py
+++ test_search_google.py
@@ -1,20 +1,18 @@
+
 from selene import browser, have, be
 
 
 def test_captcha_should_be_shown(log_test, open_google):
-    browser.element('[name="q"]').should(be.visible)
-    browser.element('[name="q"]').type('qa.guru')
+    browser.element('[name="q"]').should(be.visible).type('qa.guru')
     browser.element('[name="btnK"]').should(be.visible).should(be.clickable)
     browser.element('[name="btnK"]').click()
     browser.element('html').should(have.text('Об этой странице'))
 
 
 
-def test_other_site_show(log_test, open_other_browser):
-    browser.should(have.script_returned(True, 'return document.readyState === "complete"'))
-    browser.element('[placeholder="Поиск без отслеживания"]').should(have.attribute('placeholder').value('Поиск без отслеживания'))
-    browser.element('[data-testid="hero"]').should(have.text('Подробнее'))
-    browser.element('[id="searchbox_input"]').type('%*(&*^%^$%$&%^(*^*(&%^#$%^*$(^&^&%^$^&*%^&*(^')
+def test_other_site_show(log_test, open_duck):
+    #browser.element('[placeholder="Поиск без отслеживания"]').should(have.attribute('placeholder').value('Поиск без отслеживания'))
+    browser.element('[id="searchbox_input"]').should(be.visible).type('%*(&*^%^$%$&%^(*^*(&%^#$%^*$(^&^&%^$^&*%^&*(^')
     browser.element('[type="submit"]').should(be.visible).should(be.clickable)
     browser.element('[type="submit"]').click()
     browser.element('[id=react-layout]').should(have.text('По запросу «%*(&*^%^$%$&%^(*^*(&%^#$%^*$(^&^&%^$^&*%^&*(^» ничего не найдено.'))
